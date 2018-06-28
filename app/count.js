54fb9dcdb606e5a3782762b29359b075f78f683f
David Sanchez <sanchez.dav90@gmail.com>
2018-06-28 10:06:58 -0500

    Solve counter module

--- app/count.js
+++ app/count.js
@@ -2,6 +2,19 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.countAnswers = {
   count: function (start, end) {
+    return (function run() {
+      let timeOut;
+      console.log(start++);
 
+      if (start <= end) {
+        timeOut = setTimeout(run, 100);
+      }
+
+      return {
+        cancel() {
+          clearTimeout(timeOut);
+        }
+      };
+    })();
   }
 };
