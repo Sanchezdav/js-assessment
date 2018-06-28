3c3e2de3701f63e5555d9653721054f76b169123
David Sanchez <sanchez.dav90@gmail.com>
2018-06-28 09:16:13 -0500

    Solve async module

--- app/async.js
+++ app/async.js
@@ -2,10 +2,21 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.asyncAnswers = {
   async: function(value) {
+    const promise = new Promise((resolve, reject) => {
+      setTimeout(() => { resolve(value); }, 250)
+    })
 
+    return promise
   },
 
   manipulateRemoteData: function(url) {
+    const promise = new Promise((resolve, reject) => {
+      $.get(url, (data) => {
+        const people = data.people.map(person => person.name).sort()
+        resolve(people)
+      })
+    })
 
+    return promise
   }
 };
