387bc89b770273be6640b4017476baae6526bda5
David Sanchez <sanchez.dav90@gmail.com>
2018-07-17 21:30:17 -0500

    Solve numbers module

--- app/numbers.js
+++ app/numbers.js
@@ -2,18 +2,23 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.numbersAnswers = {
   valueAtBit: function(num, bit) {
-
+    return 1 & (num >> (bit - 1));
   },
 
   base10: function(str) {
-
+    return parseInt(str, 2);
   },
 
   convertToBinary: function(num) {
-
+    return (num >>> 0).toString(2);
   },
 
   multiply: function(a, b) {
+    var commonMultiplier = 1000000;
 
+    a *= commonMultiplier;
+    b *= commonMultiplier;
+
+    return (a * b) / (commonMultiplier * commonMultiplier);
   }
 };
