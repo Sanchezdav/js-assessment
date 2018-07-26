1c5ffa809abbacb4b6983dd40a7bd1377ba0a4bf
David Sanchez <sanchez.dav90@gmail.com>
2018-07-25 22:15:20 -0500

    Solves objects module

--- app/objects.js
+++ app/objects.js
@@ -2,14 +2,22 @@ exports = typeof window === 'undefined' ? global : window;
 
 exports.objectsAnswers = {
   alterContext: function(fn, obj) {
-
+    return fn.apply(obj)
   },
 
   alterObjects: function(constructor, greeting) {
-
+    constructor.prototype.greeting = greeting;
   },
 
   iterate: function(obj) {
+    let result = [];
 
+    for (let prop in obj) {
+      if (obj.hasOwnProperty(prop)) {
+        result.push(`${prop}: ${obj[prop]}`);
+      }
+    }
+
+    return result;
   }
 };
